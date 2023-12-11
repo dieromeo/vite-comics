bb9c707ff4ff620daf44002b4efc589e38a9801c
dieromeo <diegoromeo99@icloud.com>
2023-12-11 14:39:16 +0100

    feat: cards

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'Card',
+    props: {
+        image: String,
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-image">
+            <img :src="image" alt="">
+        </div>
+        <div class="card-title">
+            <h4>{{ title.toUpperCase() }}</h4>
+        </div>
+    </div>
+</template>
+
+<style>
+.card-image img {
+    width: 100%;
+    display: block;
+}
+</style>
