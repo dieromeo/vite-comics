33484fc8c554d57a50037abcae4651acb5a4523d
dieromeo <diegoromeo99@icloud.com>
2023-12-11 17:41:22 +0100

    feat: content

---
+++ src/components/BlueButton.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'BlueButton'
+}
+</script>
+
+<template>
+    <button> Load more </button>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/main.scss' as *;
+
+button {
+    all: unset;
+    background-color: $button-br;
+    padding: 10px;
+    color: $titles;
+    padding: 10px 60px;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-size: 14;
+    font-weight: bold;
+    margin-bottom: 20px;
+
+}
+</style>
